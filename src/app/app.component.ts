6f570ae452dd933bddec4ed9e57aa5bc749ba9cb
Jatangitindh <tandonjatan@gmail.com>
2023-12-31 16:21:05 +0530

    intial component working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodoButtonComponent } from './todo-button/todo-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,TodoButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todolist';
 }
+
+
+
+
+
+
+
+
+
+
---
+++ src/app/todo-button/todo-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-button.component.html',
+  styleUrl: './todo-button.component.css'
+})
+export class TodoButtonComponent {
+
+}
